c00e84b50f1419c27167e82a3d4c22b73f345dc2
Shamsher Khan <168928475+Shamsherkhan7549@users.noreply.github.com>
2025-03-11 18:01:44 +0530

    image location changed

--- Pratham-online/src/component/Navbar.jsx
+++ Pratham-online/src/component/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
         <nav className="navbar navbar-expand-sm bg-body-white px-5 py-3 shadow-sm">
   <div className="container-fluid">
     <a className="navbar-brand" href="/">
-        <img className='w-50 w-sm-75 w-md-100' src="../src/assets/pratham.png" alt="compny-logo" />
+        <img className='w-50 w-sm-75 w-md-100' src="../public/pratham.png" alt="compny-logo" />
     </a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
--- Pratham-online/src/pages/Home.jsx
+++ Pratham-online/src/pages/Home.jsx
@@ -9,24 +9,24 @@ const Home = () => {
        <div className='banner-program'>
             <Hero 
             id={1}
-            img1={"../src/assets/ubanner3.jpeg"}
-            img2={"../src/assets/b12.jpg"}
-            img3={"../src/assets/b13.jpg"}
+            img1={"../public/ubanner3.jpeg"}
+            img2={"../public/b12.jpg"}
+            img3={"../public/b13.jpg"}
             />
             <Program/>
        </div>
         <Hero 
             id={2}
-            img1={"../src/assets/newbp.png"}
-            img2={"../src/assets/b12.jpg"}
-            img3={"../src/assets/b13.jpg"}
+            img1={"../public/newbp.png"}
+            img2={"../public/b12.jpg"}
+            img3={"../public/b13.jpg"}
             />
        <OptionContainer/>
        <Hero 
             id={3}
-            img1={"../src/assets/bannerselectionipm1.jpg"}
-            img2={"../src/assets/b8.jpg"}
-            img3={"../src/assets/b4.jpg"}
+            img1={"../public/bannerselectionipm1.jpg"}
+            img2={"../public/b8.jpg"}
+            img3={"../public/b4.jpg"}
             />
 
           <div className=" WhyPratham">
